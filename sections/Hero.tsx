241759cd184b600e01d5417ce66ba09fe280dc70
shigure-develop-user-interface <sutera@na-cat.com>
2025-01-21 15:36:04 +0000

    Publish 2 from staging into origin/main

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,6 +1,84 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 
+import process from 'node:process'
+
+function currentPlatform(): string {
+  let os = null;
+
+  switch (process.platform) {
+    case 'android':
+      switch (process.arch) {
+        case 'arm':
+          return 'android-arm-eabi';
+        case 'arm64':
+          return 'android-arm64';
+      }
+      os = 'Android';
+      break;
+
+    case 'win32':
+      switch (process.arch) {
+        case 'x64':
+          return 'win32-x64-msvc'
+        case 'arm64':
+          return 'win32-arm64-msvc';
+        case 'ia32':
+          return 'win32-ia32-msvc';
+      }
+      os = 'Windows';
+      break;
+
+    case 'darwin':
+      switch (process.arch) {
+        case 'x64':
+          return 'darwin-x64';
+        case 'arm64':
+          return 'darwin-arm64';
+      }
+      os = 'macOS';
+      break;
+
+    case 'linux':
+      switch (process.arch) {
+        case 'x64':
+        case 'arm64':
+          return isGlibc()
+            ? `linux-${process.arch}-gnu`
+            : `linux-${process.arch}-musl`;
+        case 'arm':
+          return 'linux-arm-gnueabihf';
+      }
+      os = 'Linux';
+      break;
+
+    case 'freebsd':
+      if (process.arch === 'x64') {
+        return 'freebsd-x64';
+      }
+      os = 'FreeBSD';
+      break;
+  }
+
+  return `unsupported: os=${os} process.platform=${process.platform} process.arch=${process.arch}`
+}
+
+function isGlibc(): boolean {
+  // Cast to unknown to work around a bug in the type definition:
+  // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/40140
+  const report: unknown = process.report?.getReport();
+
+  if ((typeof report !== 'object') || !report || (!('header' in report))) {
+    return false;
+  }
+
+  const header = report.header;
+
+  return (typeof header === 'object') &&
+    !!header &&
+    ('glibcVersionRuntime' in header);
+}
+
 export interface CTA {
   id?: string;
   href: string;
@@ -68,7 +146,9 @@ export default function HeroFlats({
             >
             </div>
             <p class="leading-[150%] md:text-md text-lg">
-              {description} {Deno.version.deno}
+              {description}<br/>
+              Deno:{Deno.version.deno}<br/>
+              Platform: {currentPlatform()}
             </p>
             {cta && cta.length > 0 &&
               (
